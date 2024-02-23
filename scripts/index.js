571493d86b7e6f7c033016800724f23d3250f596
Christoph Hehl <chehl@marco.de>
2024-02-23 16:41:40 +0100

    added bootstrap and changed a few things

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,3 @@
-function correctCSSForMobileDevices() {
-    if (!isUsingMobile()) return;
-    document.getElementById('myname').style.fontSize='6vh';
-    const logos = document.getElementsByClassName("logo");
-    for (let i = 0; i < logos.length; i++) {
-        logos[i].style.height='6vh';
-    }
-}
-
-function isUsingMobile() {
-    return navigator.userAgent.includes("Mobile");
-}
-
 function copyDiscordName() {
     const discordName = "skrik"
     /* Copy the text inside the text field */
